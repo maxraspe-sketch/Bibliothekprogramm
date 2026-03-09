9ffef6ebfdd0fa19a9642f273eaccac3b6c06c1b
Max_Raspe <max.raspe-mb25@mb.dhbw-ravensburg.de>
2026-03-09 18:14:42 +0100

    Structures erstellen

---
+++ Library/Projektabgabe/Program.vb
@@ -0,0 +1,16 @@
+Option Strict On
+
+Module Program
+
+    Structure UserRecord
+        Dim Id As String
+        Dim Name As String
+    End Structure
+
+    Structure BookRecord
+        Dim Isbn As String
+        Dim Title As String
+        Dim Author As String
+        Dim Status As String
+        Dim BorrowerId As String
+    End Structure
